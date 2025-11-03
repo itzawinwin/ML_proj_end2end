8d1887ef6ca6d7a2537539e02c6e2d61e00c66a2
Its-a-Win-Win <thngng.eng@gmail.com>
2025-11-02 23:35:49 -0500

    Data transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,6 +5,9 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transfomation import DataTranformation
+from src.components.data_transfomation import DataTransformationConfig
+
 
 @dataclass
 class DataIngestionConfig:
@@ -35,10 +38,14 @@ class DataIngestion:
                     self.ingestion_config.test_data_path)
             
             
-            
         except Exception as e:
             raise CustomException(e,sys)
 
 if __name__ == "__main__":
     obj= DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+    
+    data_transformation = DataTranformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
+    
+    
---
+++ src/pipeline/train_pipeline.py
@@ -0,0 +1,1 @@
+# call all the components
